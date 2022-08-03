8a1881afb8f31c67d6d9ac7ff6bfc167c6e6a946
Austin Miller <austinrmiller1991@gmail.com>
2022-08-03 11:01:40 -0700

    Adds hyperping.io integration (#44)

---
+++ app/models/pager_tree/integrations/hyperping/v3.rb
@@ -0,0 +1,60 @@
+module PagerTree::Integrations
+  class Hyperping::V3 < Integration
+    OPTIONS = []
+    store_accessor :options, *OPTIONS.map { |x| x[:key] }.map(&:to_s), prefix: "option"
+
+    after_initialize do
+    end
+
+    def adapter_supports_incoming?
+      true
+    end
+
+    def adapter_supports_outgoing?
+      false
+    end
+
+    def adapter_incoming_can_defer?
+      true
+    end
+
+    def adapter_thirdparty_id
+      adapter_incoming_request_params.dig("check", "url")
+    end
+
+    def adapter_action
+      event = adapter_incoming_request_params.dig("event")
+      case event
+      when "check.down"
+        :create
+      when "check.up"
+        :resolve
+      else
+        :other
+      end
+    end
+
+    def adapter_process_create
+      Alert.new(
+        title: _title,
+        thirdparty_id: adapter_thirdparty_id,
+        dedup_keys: [adapter_thirdparty_id],
+        additional_data: _additional_datums
+      )
+    end
+
+    private
+
+    def _title
+      adapter_incoming_request_params.dig("check", "url") + " is DOWN"
+    end
+
+    def _additional_datums
+      [
+        AdditionalDatum.new(format: "link", label: "URL", value: adapter_incoming_request_params.dig("check", "url")),
+        AdditionalDatum.new(format: "text", label: "Status Code", value: adapter_incoming_request_params.dig("check", "status")),
+        AdditionalDatum.new(format: "datetime", label: "Down Since", value: Time.at(adapter_incoming_request_params.dig("check", "date") / 1000).utc.to_datetime)
+      ]
+    end
+  end
+end
---
+++ test/models/pager_tree/integrations/hyperping/v3_test.rb
@@ -0,0 +1,91 @@
+require "test_helper"
+
+module PagerTree::Integrations
+  class Hyperping::V3Test < ActiveSupport::TestCase
+    include Integrateable
+
+    setup do
+      @integration = pager_tree_integrations_integrations(:hyperping_v3)
+
+      @create_request = {
+        event: "check.down",
+        check: {
+          url: "https://js.stripe.com/v3/",
+          status: 502,
+          down: true,
+          date: 1556506024291,
+          downtime: 1
+        },
+        pings: [
+          {
+            original: true,
+            location: "london",
+            status: 502,
+            statusMessage: "Bad Gateway"
+          },
+          {
+            original: false,
+            location: "paris",
+            status: 502,
+            statusMessage: "Bad Gateway"
+          },
+          {
+            original: false,
+            location: "frankfurt",
+            status: 502,
+            statusMessage: "Bad Gateway"
+          }
+        ]
+      }.with_indifferent_access
+
+      @resolve_request = @create_request.deep_dup
+      @resolve_request[:event] = "check.up"
+
+      @other_request = @create_request.deep_dup
+      @other_request[:event] = "check.baaad"
+    end
+
+    test "sanity" do
+      assert @integration.adapter_supports_incoming?
+      assert @integration.adapter_incoming_can_defer?
+      assert_not @integration.adapter_supports_outgoing?
+      assert @integration.adapter_show_alerts?
+      assert @integration.adapter_show_logs?
+      assert_not @integration.adapter_show_outgoing_webhook_delivery?
+    end
+
+    test "adapter_actions" do
+      @integration.adapter_incoming_request_params = @create_request
+      assert_equal :create, @integration.adapter_action
+
+      @integration.adapter_incoming_request_params = @resolve_request
+      assert_equal :resolve, @integration.adapter_action
+
+      @integration.adapter_incoming_request_params = @other_request
+      assert_equal :other, @integration.adapter_action
+    end
+
+    test "adapter_thirdparty_id" do
+      @integration.adapter_incoming_request_params = @create_request
+      assert_equal @create_request.dig("check", "url"), @integration.adapter_thirdparty_id
+    end
+
+    test "adapter_process_create" do
+      @integration.adapter_incoming_request_params = @create_request
+
+      true_alert = Alert.new(
+        title: @create_request.dig("check", "url") + " is DOWN",
+        urgency: nil,
+        thirdparty_id: @create_request.dig("check", "url"),
+        dedup_keys: [@create_request.dig("check", "url")],
+        additional_data: [
+          AdditionalDatum.new(format: "link", label: "URL", value: @create_request.dig("check", "url")),
+          AdditionalDatum.new(format: "text", label: "Status Code", value: @create_request.dig("check", "status")),
+          AdditionalDatum.new(format: "datetime", label: "Down Since", value: Time.at(@create_request.dig("check", "date") / 1000).utc.to_datetime)
+        ]
+      )
+
+      assert_equal true_alert.to_json, @integration.adapter_process_create.to_json
+    end
+  end
+end
