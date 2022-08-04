298d70b93c6c33ece480b94b481a3f5c1c88dda1
Austin Miller <austinrmiller1991@gmail.com>
2022-08-04 16:48:55 -0700

    Adds Uptime.com integration (#59)

---
+++ app/models/pager_tree/integrations/uptime/v3.rb
@@ -0,0 +1,68 @@
+module PagerTree::Integrations
+  class Uptime::V3 < Integration
+    OPTIONS = []
+    store_accessor :options, *OPTIONS.map { |x| x[:key] }.map(&:to_s), prefix: "option"
+
+    after_initialize do
+    end
+
+    def adapter_supports_incoming?
+      true
+    end
+
+    def adapter_supports_outgoing?
+      false
+    end
+
+    def adapter_incoming_can_defer?
+      true
+    end
+
+    def adapter_thirdparty_id
+      adapter_incoming_request_params.dig("data", "service", "id")
+    end
+
+    def adapter_action
+      case adapter_incoming_request_params.dig("event")
+      when "alert_raised"
+        :create
+      when "alert_cleared"
+        :resolve
+      else
+        :other
+      end
+    end
+
+    def adapter_process_create
+      Alert.new(
+        title: _title,
+        description: _description,
+        thirdparty_id: adapter_thirdparty_id,
+        dedup_keys: [adapter_thirdparty_id],
+        additional_data: _additional_datums,
+        tags: _tags
+      )
+    end
+
+    private
+
+    def _title
+      adapter_incoming_request_params.dig("data", "service", "display_name")
+    end
+
+    def _description
+      adapter_incoming_request_params.dig("data", "alert", "output")
+    end
+
+    def _tags
+      Array(adapter_incoming_request_params.dig("data", "service", "tags")).compact_blank.uniq
+    end
+
+    def _additional_datums
+      [
+        AdditionalDatum.new(format: "text", label: "Service Name", value: adapter_incoming_request_params.dig("data", "service", "name")),
+        AdditionalDatum.new(format: "text", label: "Service Address", value: adapter_incoming_request_params.dig("data", "service", "msp_address"))
+      ]
+    end
+  end
+end
