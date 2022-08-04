837cdc63462ce2ac2014959a0d8e74f50c31f278
Austin Miller <austinrmiller1991@gmail.com>
2022-08-04 16:09:43 -0700

    Adds Typeform integration

---
+++ app/models/pager_tree/integrations/typeform/v3.rb
@@ -0,0 +1,43 @@
+module PagerTree::Integrations
+  class Typeform::V3 < Integration
+    OPTIONS = []
+    store_accessor :options, *OPTIONS.map { |x| x[:key] }.map(&:to_s), prefix: "option"
+
+    after_initialize do
+    end
+
+    def adapter_supports_incoming?
+      true
+    end
+
+    def adapter_supports_outgoing?
+      false
+    end
+
+    def adapter_incoming_can_defer?
+      true
+    end
+
+    def adapter_thirdparty_id
+      adapter_incoming_request_params.dig("form_response", "token")
+    end
+
+    def adapter_action
+      :create
+    end
+
+    def adapter_process_create
+      Alert.new(
+        title: _title,
+        thirdparty_id: adapter_thirdparty_id,
+        dedup_keys: [adapter_thirdparty_id]
+      )
+    end
+
+    private
+
+    def _title
+      [adapter_incoming_request_params.dig("form_response", "definition", "title"), adapter_incoming_request_params.dig("form_response", "submitted_at")].compact_blank.join(": ")
+    end
+  end
+end
