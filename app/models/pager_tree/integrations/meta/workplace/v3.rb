71f81e3ab440cf05b3580a7c647e59b56d31501f
Austin Miller <austinrmiller1991@gmail.com>
2023-11-28 15:04:40 -0700

    Meta Workplace Integration (#114)
    * Adding Meta Workplace Integration (implementation)
    
    * TODO: Tests for Meta Workplace
    
    * Adding Meta Workplace Integration (implementation)
    
    * TODO: Tests for Meta Workplace
    
    * Add the option for outgoing rules for the meta workplace integration
    
    * Simplifies the Meta Workplace integration and adds the comment created feature
    
    * Adding tests for meta workplace integration
    
    * Segregate the meta workplace data by integration.
    
    * Add some better error handling in the Workplace integration
    
    * Trigger off the alert assigned event, not the created event.

---
+++ app/models/pager_tree/integrations/meta/workplace/v3.rb
@@ -0,0 +1,230 @@
+module PagerTree::Integrations
+  class Meta::Workplace::V3 < Integration
+    OPTIONS = [
+      {key: :incoming_enabled, type: :boolean, default: false},
+      {key: :outgoing_enabled, type: :boolean, default: true},
+      {key: :access_token, type: :string, default: nil},
+      {key: :app_secret, type: :string, default: nil},
+      {key: :group_id, type: :string, default: nil},
+      {key: :outgoing_rules, type: :string, default: nil}
+    ]
+    store_accessor :options, *OPTIONS.map { |x| x[:key] }.map(&:to_s), prefix: "option"
+
+    validates :option_incoming_enabled, inclusion: {in: [true, false]}
+    validates :option_outgoing_enabled, inclusion: {in: [true, false]}
+    validates :option_access_token, presence: true
+    # validates :option_app_secret, presence: true
+    validates :option_group_id, presence: true
+
+    after_initialize do
+      self.option_incoming_enabled = false if option_incoming_enabled.nil?
+      self.option_outgoing_enabled = true if option_outgoing_enabled.nil?
+      self.option_outgoing_rules ||= ""
+    end
+
+    def endpoint
+      super + "/g"
+    end
+
+    def adapter_should_block_incoming?(request)
+      should_block = false
+
+      if option_app_secret.present? && request.headers["x-hub-signature-256"].present?
+        signature = request.headers["x-hub-signature-256"].delete_prefix("sha256=")
+        data = request.body.read
+        digest = OpenSSL::HMAC.hexdigest("SHA256", option_app_secret, data)
+        should_block = signature != digest
+      end
+
+      should_block
+    end
+
+    def adapter_supports_incoming?
+      # TODO - change this to true once we are ready to support this. The code currently works, but we don't want to confuse our users.
+      # Let's work with them to see what they want the functionality to look like
+      false
+    end
+
+    def adapter_supports_outgoing?
+      true
+    end
+
+    def adapter_show_outgoing_webhook_delivery?
+      true
+    end
+
+    def adapter_incoming_can_defer?
+      false
+    end
+
+    def adapter_supports_title_template?
+      false
+    end
+
+    def adapter_supports_description_template?
+      false
+    end
+
+    def adapter_thirdparty_id
+      [_entry&.dig("id"), _entry&.dig("time")].compact_blank.join("-")
+    end
+
+    def adapter_action
+      if option_incoming_enabled && adapter_incoming_request_params.dig("object") == "page" && _entry_changes&.dig("field") == "mention"
+        :create
+      else
+        :other
+      end
+    end
+
+    def adapter_response_incoming
+      if adapter_incoming_request_params["hub.mode"] == "subscribe"
+        adapter_controller&.render(plain: adapter_incoming_request_params["hub.challenge"])
+      else
+        adapter_controller&.head(:ok)
+      end
+    end
+
+    def adapter_process_create
+      # send back an ok
+      post_id = _entry_changes&.dig("value", "post_id")
+      _post_comment(post_id, "Ok") if post_id.present?
+
+      Alert.new(
+        title: _title,
+        thirdparty_id: adapter_thirdparty_id
+      )
+    end
+
+    def adapter_process_other
+    end
+
+    def adapter_outgoing_interest?(event_name)
+      option_outgoing_enabled && [
+        "alert_assigned",
+        "alert_acknowledged",
+        "alert_rejected",
+        "alert_resolved",
+        "alert_dropped",
+        "alert_handoff",
+        "comment_created"
+      ].include?(event_name.to_s)
+    end
+
+    def adapter_process_outgoing
+      event_type = adapter_outgoing_event.event_name.to_s
+      message = _generate_message(event_type)
+
+      return unless message.present?
+
+      if event_type == "alert_assigned"
+        _post_message(option_group_id, message)
+      else
+        post_id = _alert.meta["#{id}_meta_workplace_post_id"]
+
+        if post_id.blank?
+          outgoing_webhook_delivery_id = _alert.meta["#{id}_meta_workplace_outgoing_webhook_delivery_id"]
+          outgoing_webhook_delivery = OutgoingWebhookDelivery.find_by(id: outgoing_webhook_delivery_id)
+          post_id = begin
+            JSON.parse(outgoing_webhook_delivery.responses.first.dig("body"))["id"]
+          rescue
+            nil
+          end
+
+          if post_id.present?
+            _alert.meta["#{id}_meta_workplace_post_id"] = post_id
+            _alert.save!
+          end
+        end
+
+        _post_comment(post_id, message) if post_id.present?
+      end
+    rescue => e
+      Rails.logger.error "[Workplace] Error processing outgoing event: #{e.message}"
+    end
+
+    private
+
+    # INCOMING
+    def _entry
+      @_entry ||= adapter_incoming_request_params&.dig("entry", 0)
+    end
+
+    def _entry_changes
+      @_entry_changes ||= _entry&.dig("changes", 0)
+    end
+
+    def _title
+      _entry_changes&.dig("value", "message")
+    end
+
+    def _additional_datums
+      []
+    end
+
+    # OUTGOING
+    def _alert
+      @_alert ||= adapter_outgoing_event.alert
+    end
+
+    def _post_message(group_id, message)
+      # This operation needs to happen immediately because we need to attach the post_id to the alert
+      url = "https://graph.facebook.com/#{group_id}/feed?message=#{CGI.escape(message)}&formatting=MARKDOWN"
+      outgoing_webhook_delivery = _send_outgoing(url)
+      _alert.meta["#{id}_meta_workplace_outgoing_webhook_delivery_id"] = outgoing_webhook_delivery.id
+      _alert.save!
+
+      outgoing_webhook_delivery
+    end
+
+    def _post_comment(post_id, message)
+      url = "https://graph.facebook.com/#{post_id}/comments?message=#{CGI.escape(message)}"
+      _send_outgoing(url)
+    end
+
+    def _send_outgoing(url)
+      outgoing_webhook_delivery = OutgoingWebhookDelivery.factory(
+        resource: self,
+        url: url,
+        options: {
+          headers: {
+            "Authorization" => "Bearer #{option_access_token}"
+          }
+        }
+      )
+      outgoing_webhook_delivery.save!
+      outgoing_webhook_delivery.deliver_later
+
+      outgoing_webhook_delivery
+    end
+
+    def _generate_message(event_name)
+      case event_name
+      when "alert_assigned"
+        if _alert.incident?
+          "[Incident ##{_alert.tiny_id}](#{Rails.application.routes.url_helpers.try(:alert_url, _alert, script_name: "/#{_alert.account_id}")}) [#{_alert.incident_severity.upcase.dasherize}] #{_alert.incident_message} - #{_alert.title}"
+        else
+          "[Alert ##{_alert.tiny_id}](#{Rails.application.routes.url_helpers.try(:alert_url, _alert, script_name: "/#{_alert.account_id}")}) #{_alert.title}"
+        end
+      when "alert_acknowledged"
+        acknowledger = adapter_outgoing_event.account_user || adapter_outgoing_event.team
+        acknowledger&.name&.present? ? "Acknowledged by #{acknowledger.name}" : "Acknowledged"
+      when "alert_rejected"
+        rejecter = adapter_outgoing_event.account_user
+        rejecter&.name&.present? ? "Rejected by #{rejecter.name}" : "Rejected"
+      when "alert_resolved"
+        resolver = adapter_outgoing_event.account_user || adapter_outgoing_event.team
+        resolver&.name&.present? ? "Resolved by #{resolver.name}" : "Resolved"
+      when "alert_dropped"
+        "Dropped"
+      when "alert_handoff"
+        handoff = adapter_outgoing_event.handoff
+        "Handed off from #{handoff.source.name} to #{handoff.destination.name}"
+      when "comment_created"
+        comment = adapter_outgoing_event.comment
+        commenter = comment.account_user&.name || "PagerTree"
+        "#{commenter} commented: \"#{comment.body.to_plain_text}\""
+      end
+    end
+  end
+end
---
+++ test/models/pager_tree/integrations/meta/workplace/v3_test.rb
@@ -0,0 +1,121 @@
+require "test_helper"
+
+module PagerTree::Integrations
+  class Meta::Workplace::V3Test < ActiveSupport::TestCase
+    include Integrateable
+    include ActiveJob::TestHelper
+
+    setup do
+      @integration = pager_tree_integrations_integrations(:meta_workplace_v3)
+
+      @alert = JSON.parse({
+        id: "01G9ZET2HZSTA9B0YDAB9G7XPZ",
+        account_id: "01G9ZDGQ0NYAF6E1M3C6FAYDV5",
+        prefix_id: "alt_K22OuvPYNmCyvJ",
+        tiny_id: 22,
+        meta: {},
+        source: {
+          name: "Joe Bob"
+        },
+        title: "new alert",
+        status: "acknowledged",
+        urgency: "medium",
+        created_at: "2022-08-08T19:27:20.127Z",
+        updated_at: "2022-08-08T19:27:49.256Z",
+        incident: false,
+        incident_severity: "sev_1",
+        incident_message: "",
+        alert_destinations: [
+          {
+            destination: {
+              name: "Team Bobcats"
+            }
+          }
+        ]
+      }.to_json, object_class: OpenStruct)
+
+      @alert.created_at = @alert.created_at.to_datetime
+      @alert.updated_at = @alert.updated_at.to_datetime
+
+      @expected_payload = nil
+    end
+
+    def expected_message_url(group_id, message)
+      "https://graph.facebook.com/#{group_id}/feed?message=#{CGI.escape(message)}&formatting=MARKDOWN"
+    end
+
+    def expected_comment_url(post_id, message)
+      "https://graph.facebook.com/#{post_id}/comments?message=#{CGI.escape(message)}"
+    end
+
+    test "sanity" do
+      assert_not @integration.adapter_supports_incoming?
+      assert_not @integration.adapter_incoming_can_defer?
+      assert @integration.adapter_supports_outgoing?
+      assert_not @integration.adapter_show_alerts?
+      assert @integration.adapter_show_logs?
+      assert @integration.adapter_show_outgoing_webhook_delivery?
+    end
+
+    test "outgoing_interest" do
+      assert @integration.option_outgoing_enabled
+      assert_not @integration.adapter_outgoing_interest?(:alert_open)
+      assert_not @integration.adapter_outgoing_interest?(:alert_timeout)
+      assert_not @integration.adapter_outgoing_interest?(:alert_created)
+      assert @integration.adapter_outgoing_interest?(:alert_assigned)
+      assert @integration.adapter_outgoing_interest?(:alert_acknowledged)
+      assert @integration.adapter_outgoing_interest?(:alert_rejected)
+      assert @integration.adapter_outgoing_interest?(:alert_resolved)
+      assert @integration.adapter_outgoing_interest?(:alert_dropped)
+      assert @integration.adapter_outgoing_interest?(:alert_handoff)
+      assert @integration.adapter_outgoing_interest?(:comment_created)
+    end
+
+    test "can_process_outgoing_message" do
+      data = {
+        event_name: :alert_assigned,
+        alert: @alert,
+        changes: [{
+          before: nil,
+          after: @alert.as_json
+        }],
+        outgoing_rules_data: {}
+      }
+
+      assert_no_performed_jobs
+
+      @integration.adapter_outgoing_event = OutgoingEvent.new(**data)
+      outgoing_webhook_delivery = @integration.adapter_process_outgoing
+
+      assert_enqueued_jobs 1
+
+      assert_equal expected_message_url(@integration.option_group_id, "[Alert ##{@alert.tiny_id}](#{Rails.application.routes.url_helpers.try(:alert_url, @alert, script_name: "/#{@alert.account_id}")}) #{@alert.title}"), outgoing_webhook_delivery.url
+      assert_equal :queued.to_s, outgoing_webhook_delivery.status
+      assert_equal @expected_payload.to_json, outgoing_webhook_delivery.body.to_json
+    end
+
+    test "can_process_outgoing_comment" do
+      @alert.meta["#{@integration.id}_meta_workplace_post_id"] = "1234"
+      data = {
+        event_name: :alert_resolved,
+        alert: @alert,
+        changes: [{
+          before: nil,
+          after: @alert.as_json
+        }],
+        outgoing_rules_data: {}
+      }
+
+      assert_no_performed_jobs
+
+      @integration.adapter_outgoing_event = OutgoingEvent.new(**data)
+      outgoing_webhook_delivery = @integration.adapter_process_outgoing
+
+      assert_enqueued_jobs 1
+
+      assert_equal expected_comment_url(@alert.meta["#{@integration.id}_meta_workplace_post_id"], "Resolved"), outgoing_webhook_delivery.url
+      assert_equal :queued.to_s, outgoing_webhook_delivery.status
+      assert_equal @expected_payload.to_json, outgoing_webhook_delivery.body.to_json
+    end
+  end
+end
