29a2df80f0ba801ff2c2aa1cb9b50919cb49fc41
Austin Miller <austinrmiller1991@gmail.com>
2025-02-11 13:52:34 -0700

    Refactoring the outgoing wehook delivery

--- app/models/pager_tree/integrations/outgoing_webhook_delivery.rb
+++ app/models/pager_tree/integrations/outgoing_webhook_delivery.rb
@@ -1,22 +1,7 @@
 module PagerTree::Integrations
-  class OutgoingWebhookDelivery < PagerTree::Integrations.outgoing_webhook_delivery_parent_class.constantize
-    self.table_name = PagerTree::Integrations.outgoing_webhook_delivery_table_name
-
-    serialize :data, JSON
-    encrypts :data
-
-    store_accessor :data, *[:url, :body, :auth, :options].map(&:to_s)
-
-    HTTP_OPTIONS = {
-      headers: {"Content-Type" => "application/json"},
-      timeout: 15
-    }
-
-    belongs_to :resource, polymorphic: true
-    enum status: {queued: 0, sent: 1, success: 2, failure: 3, retrying: 4, cancelled: 5, stored: 6, insufficent_funds: 7}
-
+  class OutgoingWebhookDelivery
     def self.factory(**params)
-      PagerTree::Integrations.outgoing_webhook_delivery_factory_class.constantize.new(**params)
+      PagerTree::Integrations.outgoing_webhook_delivery_factory_class.constantize.factory(**params)
     end
   end
 end
--- lib/pager_tree/integrations.rb
+++ lib/pager_tree/integrations.rb
@@ -11,14 +11,8 @@ module PagerTree
     mattr_accessor :integration_parent_class
     @@integration_parent_class = "ApplicationRecord"
 
-    mattr_accessor :outgoing_webhook_delivery_parent_class
-    @@outgoing_webhook_delivery_parent_class = "ApplicationRecord"
-
     mattr_accessor :outgoing_webhook_delivery_factory_class
-    @@outgoing_webhook_delivery_factory_class = "PagerTree::Integrations::OutgoingWebhookDelivery::HookRelay"
-
-    mattr_accessor :outgoing_webhook_delivery_table_name
-    @@outgoing_webhook_delivery_table_name = "pager_tree_integrations_outgoing_webhook_deliveries"
+    @@outgoing_webhook_delivery_factory_class = "::OutgoingWebhookDeliv"
 
     mattr_accessor :integration_email_v3_domain
     @@integration_email_v3_domain = "alerts.pagertree.com"
