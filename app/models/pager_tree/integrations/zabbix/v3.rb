cff95d5616db4147894893b6c0e76bfd801185ae
Austin Miller <austinrmiller1991@gmail.com>
2025-08-21 07:49:02 -0700

    Adding the Zabbix adapter (#138)

---
+++ app/models/pager_tree/integrations/zabbix/v3.rb
@@ -0,0 +1,95 @@
+module PagerTree::Integrations
+  class Zabbix::V3 < Integration
+    OPTIONS = [
+      {key: :map_urgency, type: :boolean, default: false}
+    ]
+    store_accessor :options, *OPTIONS.map { |x| x[:key] }.map(&:to_s), prefix: "option"
+
+    validates :option_map_urgency, inclusion: {in: [true, false]}
+
+    after_initialize do
+      self.option_map_urgency ||= false
+    end
+
+    def adapter_supports_incoming?
+      true
+    end
+
+    def adapter_supports_outgoing?
+      false
+    end
+
+    def adapter_incoming_can_defer?
+      true
+    end
+
+    def adapter_thirdparty_id
+      adapter_incoming_request_params.dig("trigger_id").presence || adapter_incoming_request_params.dig("event_id").presence || ULID.generate
+    end
+
+    def adapter_action
+      case adapter_incoming_request_params.dig("event_value")
+      when "{EVENT.VALUE}" then :create # testing scenario
+      when "1" then :create
+      when "0" then :resolve
+      else :other
+      end
+    end
+
+    def adapter_process_create
+      Alert.new(
+        title: _title,
+        description: _description,
+        urgency: _urgency,
+        thirdparty_id: adapter_thirdparty_id,
+        dedup_keys: [],
+        tags: _tags,
+        additional_data: _additional_datums
+      )
+    end
+
+    private
+
+    def _title
+      adapter_incoming_request_params.dig("title")
+    end
+
+    def _description
+      adapter_incoming_request_params.dig("description")
+    end
+
+    def _urgency
+      if self.option_map_urgency == true
+        case adapter_incoming_request_params.dig("event_nseverity")
+        when "0" then "silent" # Not Classified
+        when "1" then "low" # Information
+        when "2" then "medium" # Warning
+        when "3" then "medium" # Average
+        when "4" then "high" # High
+        when "5" then "critical" # Disaster
+        end
+      end
+    end
+
+    def _tags
+      (adapter_incoming_request_params.dig("event_tags") || "").split(",").map(&:strip).uniq.compact
+    end
+
+    def _additional_datums
+      [
+        AdditionalDatum.new(format: "link", label: "Zabbix URL", value: _zabbix_url)
+      ]
+    end
+
+    def _zabbix_url
+      event_source = adapter_incoming_request_params.dig("event_source")
+      url = adapter_incoming_request_params.dig("zabbix_url")
+
+      if event_source == "0"
+        url = "#{url}/tr_events.php?triggerid=#{adapter_incoming_request_params.dig("trigger_id")}&eventid=#{adapter_incoming_request_params.dig("event_id")}"
+      end
+
+      url
+    end
+  end
+end
---
+++ test/models/pager_tree/integrations/zabbix/v3_test.rb
@@ -0,0 +1,73 @@
+require "test_helper"
+
+module PagerTree::Integrations
+  class Zabbix::V3Test < ActiveSupport::TestCase
+    include Integrateable
+
+    setup do
+      @integration = pager_tree_integrations_integrations(:zabbix_v3)
+
+      @create_request = {
+        event_id: "23",
+        event_source: "0",
+        event_nseverity: "4",
+        trigger_id: "25066",
+        zabbix_url: "https://abc123.zabbix.cloud",
+        title: "Problem: https://statuscode.app/ Status",
+        description: "Problem started at 17:45:41 on 2025.08.20\r\nProblem name: https://statuscode.app/ Status\r\nHost: Zabbix server\r\nSeverity: High\r\nOperational data: 500\r\nOriginal problem ID: 23\r\n",
+        event_tags: "abc,123",
+        event_value: "1"
+      }.with_indifferent_access
+
+      @resolve_request = @create_request.deep_dup
+      @resolve_request[:event_value] = "0"
+
+      @other_request = @create_request.deep_dup
+      @other_request[:event_value] = "2"
+    end
+
+    test "sanity" do
+      assert @integration.adapter_supports_incoming?
+      assert @integration.adapter_incoming_can_defer?
+      assert_not @integration.adapter_supports_outgoing?
+      assert @integration.adapter_show_alerts?
+      assert @integration.adapter_show_logs?
+      assert_not @integration.adapter_show_outgoing_webhook_delivery?
+    end
+
+    test "adapter_actions" do
+      @integration.adapter_incoming_request_params = @create_request
+      assert_equal :create, @integration.adapter_action
+
+      @integration.adapter_incoming_request_params = @resolve_request
+      assert_equal :resolve, @integration.adapter_action
+
+      @integration.adapter_incoming_request_params = @other_request
+      assert_equal :other, @integration.adapter_action
+    end
+
+    test "adapter_thirdparty_id" do
+      @integration.adapter_incoming_request_params = @create_request
+      assert_equal "25066", @integration.adapter_thirdparty_id
+    end
+
+    test "adapter_process_create" do
+      @integration.adapter_incoming_request_params = @create_request
+
+      true_alert = Alert.new(
+        title: @create_request[:title],
+        description: @create_request[:description],
+        urgency: "high",
+        thirdparty_id: @create_request[:trigger_id],
+        dedup_keys: [],
+        tags: ["abc", "123"],
+        additional_data: [
+          AdditionalDatum.new(format: "link", label: "Zabbix URL", value: "https://abc123.zabbix.cloud/tr_events.php?triggerid=25066&eventid=23")
+        ]
+      )
+
+      assert @integration.option_map_urgency
+      assert_equal true_alert.to_json, @integration.adapter_process_create.to_json
+    end
+  end
+end
