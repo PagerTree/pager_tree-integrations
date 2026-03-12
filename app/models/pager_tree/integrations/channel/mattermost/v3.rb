4e15a2404f46f9538cdcb4aeb6a5b2169ef990d6
Austin Miller <austinrmiller1991@gmail.com>
2026-03-12 16:29:13 -0700

    Fix the mattermost created at timestamp

--- app/models/pager_tree/integrations/channel/mattermost/v3.rb
+++ app/models/pager_tree/integrations/channel/mattermost/v3.rb
@@ -2,5 +2,9 @@ module PagerTree::Integrations
   class Channel::Mattermost::V3 < Channel::Slack::V3
     # we can just piggy back the Channel::Slack::V3 since Mattermost is a clone of Slack
     # both integrations have the same exact functionality
+
+    def _alert_created_at_timestamp_formatted
+      _alert.created_at.utc.iso8601.to_s
+    end
   end
 end
